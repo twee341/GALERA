bba6156a87ae5990ef0c6504e0dbc1b7908484a3
IHateDelta <dk13.danielk@gmail.com>
2025-11-29 15:27:10 +0100

    new file:   hotb_starter_code/csvfiles/clash_royale.csv 	deleted:    hotb_starter_code/csvfiles/control_raw_annotations.csv 	new file:   hotb_starter_code/csvfiles/tiktok.csv 	modified:   hotb_starter_code/data_in.py

--- hotb_starter_code/data_in.py
+++ hotb_starter_code/data_in.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import seaborn as sb
-read_csv=pd.read_csv('C:\\Users\\user\\GALERA\\hotb_starter_code\\csvfiles\\control_raw.csv')
-df=pd.DataFrame(read_csv)
+import matplotlib.pyplot as plt
+import numpy as np
+read_csv=pd.read_csv('C:\\Users\\user\\GALERA\\hotb_starter_code\\csvfiles\\tiktok.csv')
+in_df=pd.DataFrame(read_csv)
+df=in_df[["time","O1","O2","Fp1","Fp2"]]
+#sb.lineplot(df,x="time",y="O1")
+a=np.array()
+for i in range(0,len(pd["O1"])-1):
+    a.append(pd["O1"][i+1]-pd["O1"][i])
+plt.show()
+sb.lineplot(y=a)
 print(df)
