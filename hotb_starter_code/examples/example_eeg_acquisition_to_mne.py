a81650d0afc415d3a6261416ad2e1f1ff7bef71c
IHateDelta <dk13.danielk@gmail.com>
2025-11-29 23:06:27 +0100

    deleted:    dura.fif 	new file:   hotb_starter_code/__pycache__/gemini.cpython-313.pyc 	new file:   hotb_starter_code/dd.fif 	modified:   hotb_starter_code/dura.fif 	new file:   hotb_starter_code/examples/dd.fif 	renamed:    hotb_starter_code/BrainAccessSDK-windows/python_api/brainaccess/examples/example_eeg_acquisition_to_mne.py -> hotb_starter_code/examples/example_eeg_acquisition_to_mne.py 	renamed:    hotb_starter_code/BrainAccessSDK-windows/python_api/brainaccess/examples/example_eeg_processing.py -> hotb_starter_code/examples/example_eeg_processing.py 	renamed:    hotb_starter_code/BrainAccessSDK-windows/python_api/brainaccess/examples/example_impedance_measurement.py -> hotb_starter_code/examples/example_impedance_measurement.py 	renamed:    hotb_starter_code/BrainAccessSDK-windows/python_api/brainaccess/examples/example_minimal_eeg_acquisition.py -> hotb_starter_code/examples/example_minimal_eeg_acquisition.py 	new file:   hotb_starter_code/gemini.py 	modified:   hotb_starter_code/main.py 	modified:   hotb_starter_code/readfif.py 	modified:   hotb_starter_code/test.py 	new file:   hotb_starter_code/test_focus.py

--- hotb_starter_code/examples/example_eeg_acquisition_to_mne.py
+++ hotb_starter_code/examples/example_eeg_acquisition_to_mne.py
@@ -4,55 +4,34 @@ Example how to get measurements and
 save to fif format
 using acquisition class from brainaccess.utils
 
-Change Bluetooth device name
+Change Bluetooth device name (line 23)
 """
 
 import matplotlib.pyplot as plt
 import matplotlib
 import time
-import pandas as pd
 
 from brainaccess.utils import acquisition
 from brainaccess.core.eeg_manager import EEGManager
-import gemini as g
 
 matplotlib.use("TKAgg", force=True)
 
 eeg = acquisition.EEG()
 
-# define electrode locations depending on your device
-halo: dict = {
+# define electrode locations
+cap: dict = {
     0: "Fp1",
     1: "Fp2",
     2: "O1",
     3: "O2",
 }
 
-cap: dict = {
- 0: "F3",
- 1: "F4",
- 2: "C3",
- 3: "C4",
- 4: "P3",
- 5: "P4",
- 6: "O1",
- 7: "O2",
-}
-
 # define device name
 device_name = "BA HALO 089"
-def sk_lvl(sec_mne):
-    data, times = sec_mne.get_data(return_times=True)
-    df=pd.DataFrame(data)
-    df=df.transpose()
-    df=df.drop(columns=[4])
-    print(len(df))
-    return len(df)
 
 # start EEG acquisition setup
 with EEGManager() as mgr:
-
-    eeg.setup(mgr, device_name=device_name, cap=halo, sfreq=250)
+    eeg.setup(mgr, device_name=device_name, cap=cap, sfreq=250)
 
     # Start acquiring data
     eeg.start_acquisition()
@@ -61,12 +40,11 @@ with EEGManager() as mgr:
 
     start_time = time.time()
     annotation = 1
-    while time.time() - start_time < 5:
+    while time.time() - start_time < 10:
         time.sleep(1)
         # send annotation to the device
         print(f"Sending annotation {annotation} to the device")
         eeg.annotate(str(annotation))
-        x=sk_lvl(eeg.get_mne(tim=1))
         annotation += 1
 
     print("Preparing to plot data")
@@ -75,24 +53,12 @@ with EEGManager() as mgr:
     # get all eeg data and stop acquisition
     eeg.get_mne()
     eeg.stop_acquisition()
-    mgr.disconnect()
-
-# Access MNE Raw object
-mne_raw = eeg.data.mne_raw
-print(f"MNE Raw object: {mne_raw}")
-
-# Access data as NumPy arrays
-data, times = mne_raw.get_data(return_times=True)
-print(f"Data shape: {data.shape}")
+    #mgr.disconnect()
 
 # save EEG data to MNE fif format
-eeg.data.save(f'xd.fif')
+eeg.data.save(f'dd.fif')
 # Close brainaccess library
 eeg.close()
-# conversion to microvolts
-mne_raw.apply_function(lambda x: x*10**-6)
 # Show recorded data
-mne_raw.filter(1, 40).plot(scalings="auto", verbose=False)
+eeg.data.mne_raw.filter(1, 40).plot(scalings="auto", verbose=False)
 plt.show()
-
-''
--- hotb_starter_code/test_focus.py
+++ hotb_starter_code/test_focus.py
@@ -4,34 +4,55 @@ Example how to get measurements and
 save to fif format
 using acquisition class from brainaccess.utils
 
-Change Bluetooth device name (line 23)
+Change Bluetooth device name
 """
 
 import matplotlib.pyplot as plt
 import matplotlib
 import time
+import pandas as pd
 
 from brainaccess.utils import acquisition
 from brainaccess.core.eeg_manager import EEGManager
+import gemini as g
 
 matplotlib.use("TKAgg", force=True)
 
 eeg = acquisition.EEG()
 
-# define electrode locations
-cap: dict = {
+# define electrode locations depending on your device
+halo: dict = {
     0: "Fp1",
     1: "Fp2",
     2: "O1",
     3: "O2",
 }
 
+cap: dict = {
+ 0: "F3",
+ 1: "F4",
+ 2: "C3",
+ 3: "C4",
+ 4: "P3",
+ 5: "P4",
+ 6: "O1",
+ 7: "O2",
+}
+
 # define device name
-device_name = "BA HALO 001"
+device_name = "BA HALO 089"
+def sk_lvl(sec_mne):
+    data, times = sec_mne.get_data(return_times=True)
+    df=pd.DataFrame(data)
+    df=df.transpose()
+    df=df.drop(columns=[4])
+    print(len(df))
+    return len(df)
 
 # start EEG acquisition setup
 with EEGManager() as mgr:
-    eeg.setup(mgr, device_name=device_name, cap=cap, sfreq=250)
+
+    eeg.setup(mgr, device_name=device_name, cap=halo, sfreq=250)
 
     # Start acquiring data
     eeg.start_acquisition()
@@ -40,11 +61,12 @@ with EEGManager() as mgr:
 
     start_time = time.time()
     annotation = 1
-    while time.time() - start_time < 10:
+    while time.time() - start_time < 5:
         time.sleep(1)
         # send annotation to the device
         print(f"Sending annotation {annotation} to the device")
         eeg.annotate(str(annotation))
+        x=sk_lvl(eeg.get_mne(tim=1))
         annotation += 1
 
     print("Preparing to plot data")
@@ -55,10 +77,22 @@ with EEGManager() as mgr:
     eeg.stop_acquisition()
     mgr.disconnect()
 
+# Access MNE Raw object
+mne_raw = eeg.data.mne_raw
+print(f"MNE Raw object: {mne_raw}")
+
+# Access data as NumPy arrays
+data, times = mne_raw.get_data(return_times=True)
+print(f"Data shape: {data.shape}")
+
 # save EEG data to MNE fif format
-eeg.data.save(f'{time.strftime("%Y%m%d_%H%M")}-raw.fif')
+eeg.data.save(f'xd.fif')
 # Close brainaccess library
 eeg.close()
+# conversion to microvolts
+mne_raw.apply_function(lambda x: x*10**-6)
 # Show recorded data
-eeg.data.mne_raw.filter(1, 40).plot(scalings="auto", verbose=False)
+mne_raw.filter(1, 40).plot(scalings="auto", verbose=False)
 plt.show()
+
+''
