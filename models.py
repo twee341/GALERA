1b24544d0676a18474307e576b0b4894da144bae
twee341 <ignaza1337gmail.com>
2025-11-29 19:51:50 +0100

    q

--- models.py
+++ models.py
@@ -17,11 +17,20 @@ class SessionORM(Base):
     __tablename__ = 'sessions'
     'saves work sessions and neural activity'
     id: Mapped[int] = mapped_column(primary_key=True,index=True)
+    duration: Mapped[int] = mapped_column() # duration in seconds
+    neural_data: Mapped[dict] = mapped_column(JSON)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
+
+    user: Mapped["UsersORM"] = relationship(back_populates="sessions")
 
 class UsersORM(Base):
     __tablename__ = 'users'
     'saves user data'
     id: Mapped[int] = mapped_column(primary_key=True,index=True)
-    username: Mapped[str] = mapped_column(unique=True)
-    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
+
+    sessions: Mapped[list["SessionORM"]] = relationship(back_populates="user", lazy="selectin")
+
+    
